b8892d12c44c1f2665104bfb65c1db7c235e1abf
tnazarbekov <tnazarbekov@bk.ru>
2023-04-20 11:31:52 +0600

    just flask testing

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
+listofmessages = []
+
 
 @app.route('/')
 def hello_world():
     return 'hello friend!'
 
-if __name__=='__main__':
-    app.run()
+
+@app.route('/status')
+def hello_mir():
+    return {
+        'messages_count': len(listofmessages)
+    }
+
+
+@app.route('/api/Messanger',methods=['POST'])
+def SendMessage():
+    msg = request.json
+    print(msg)
+    listofmessages.append(msg)
+    msgtext = f"{msg['Username']}<{msg['TimeStamp']}>:{msg['MessageText']}"
+    print(f"Всего сообщений {len(listofmessages)}. Посланное сообщение:{msgtext}")
+    return f" Сообщение {msgtext} успепшно отправлено. Всего сообщений{len(listofmessages)}"
+
+
+@app.route('/api/Messanger/<int:id>')
+def  GetMessage(id):
+    print(id)
+    if id>=0 and id <len(listofmessages):
+        print(listofmessages[id])
+        return listofmessages[id],200
+    else:
+        return 'error 404'
+if __name__ == '__main__':
+        app.run()
